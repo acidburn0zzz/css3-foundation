f357ede3fb92c63038a5267b0774d1081af2f273
Richa Avasthi <ravasthi@crittercism.com>
2015-05-13 12:45:59 -0700

    Scratch.

---
+++ scripts/scratch.js
@@ -0,0 +1,13 @@
+/*------------------------------------------------------------------------------
+| scratch.js
+|
+| Play.
+------------------------------------------------------------------------------*/
+
+// Establish namespace. Change to match the name of your application.
+var Css3Foundation = window.Css3Foundation ? window.Css3Foundation : {};
+
+Css3Foundation.scratch = {
+    init: function() {
+    }
+};
